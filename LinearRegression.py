b5fda4e22c9f489cf6a04f05f1671b915c562a27
StrixzIV <46487993+JirayuKaewsing@users.noreply.github.com>
2021-05-26 20:38:29 +0700

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -1,20 +1,26 @@
+#import
 import tensorflow as tf
 import numpy as np
 from matplotlib import pyplot as plt
 
+#create dataset
 n = 1000
 x = np.random.rand(n)
 y = 10 * x + 7 + 0.7 * np.random.rand(n)
 plt.plot(x, y, '.')
 
+#create random weight & bias value
 w = np.random.rand()
 b = np.random.rand()
 
+#set weight & bias value for tensorflow
 w = tf.Variable(w)
 b = tf.Variable(b)
 
+#set learning rate
 lr = 0.1
 
+#doing the regression
 for epoch in range(1000):
   with tf.GradientTape() as t:
     Y = w * x + b
@@ -26,6 +32,7 @@ for epoch in range(1000):
 
   print(epoch, w.numpy(), b.numpy(), loss.numpy())
 
+#plot the result
 z = w * x + b
 
 plt.plot(x, y, '.b')
